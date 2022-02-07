5fb7f3fa2c6a5c5d708cc20763cd415d21ac536a
MunavvarSinan <munavvarsinan8@gmail.com>
2022-02-07 22:39:10 +0530

    cars and pedestrian detection

--- cars_and_pedestrian_detection/cars_and_pedestrian_detection.py
+++ cars_and_pedestrian_detection/cars_and_pedestrian_detection.py
@@ -3,11 +3,13 @@ import cv2
 # sample image file
 img_file = 'cars.jpeg'
 # sample video file
-video = cv2.VideoCapture('cars_video2.mp4')
+video = cv2.VideoCapture('cars_and_pedestrian.mp4')
 # pretrained data
-classifier = 'cars.xml'
+cars_classifier = 'cars.xml'
+pedestrian_classifier = 'haarcascade_fullbody.xml'
 # train the data
-cars_tracker = cv2.CascadeClassifier(classifier)
+cars_tracker = cv2.CascadeClassifier(cars_classifier)
+pedestrian_tracker = cv2.CascadeClassifier(pedestrian_classifier)
 # while the video is open
 while True:
     # read the video and get the frame -- read_successful will return a boolean value and frame return all frames in each loopings
@@ -21,12 +23,17 @@ while True:
         break
     # get all the frames in all sizes
     cars = cars_tracker.detectMultiScale(greyscale_frame, 1.1, 1)
+    pedestrians = pedestrian_tracker.detectMultiScale(greyscale_frame, 1.1, 1)
     # loop through the values we got from cars and form a rectangle
     for(x, y, w, h) in cars:
         # create a rectangle
+        cv2.rectangle(frame, (x +1, y + 2), (x + w, y+h), (255, 0, 0), 2 )
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+    # Draw a rectangle around pedestrians
+    for(x, y, w, h) in pedestrians:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 255), 2)
     # to create a frame
-    cv2.imshow('car_detection', frame)
+    cv2.imshow('cars_and_pedestrian_detection', frame)
     # wait the frame from closing
     key = cv2.waitKey(1)
     # close if 'q' pressed
